19b3beebd54eb84f1d16c13cb9f0d1d6172c6521
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-15 16:05:09 +0100

    add content on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import data from './data/languages'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0);
+
+
+  const handleClick = (index) => {
+    setActiveIndex(index);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h2 className='title'>Learn Web Development</h2>
+      {/*  Barra di Navigazione */}
+      <ul className="nav">
+        {data.map((language, index) => (
+          <li
+            key={language.id}
+            className={`nav-item ${activeIndex === index ? 'active' : 'hide'}`}
+            onClick={() => handleClick(index)}
+          >
+            {language.title}
+          </li>
+        ))}
+      </ul>
+      {/* Contenuto */}
+      <div className="content">
+        <h3>{data[activeIndex].title}</h3>
+        <p>{data[activeIndex].description}</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
 export default App
