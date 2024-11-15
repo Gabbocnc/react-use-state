70fb1bc351890a7962f4b519f4a59cb297019864
Gabbocnc <cianci.gabriele@hotmail.it>
2024-11-15 16:26:22 +0100

    add condition for content

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import data from './data/languages'
 import './App.css'
 
 function App() {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(null);
 
 
   const handleClick = (index) => {
@@ -28,8 +28,15 @@ function App() {
       </ul>
       {/* Contenuto */}
       <div className="content">
-        <h3>{data[activeIndex].title}</h3>
-        <p>{data[activeIndex].description}</p>
+        <div className="content-box">
+          {data[activeIndex] && (
+            <div>
+              <h3>{data[activeIndex].title}</h3>
+              <p>{data[activeIndex].description}</p>
+            </div>
+          )}
+          {!data[activeIndex] && <p>Nessun linguaggio selezionato</p>}
+        </div>
       </div>
     </div>
   );
